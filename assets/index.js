7fb3a32dccc9ce4267ffc28c432ce4b114e215f1
bryan <bryanhernandz102@gmail.com>
2022-10-28 19:45:07 -0500

    add responsive toogle interaction

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+let nav = document.querySelector("nav");
+
+// Abrir
+document.querySelector(".open").addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+});
+// cerrar
+document.querySelector(".close").addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+});
+
+// limpar
+let navs = document.getElementsByClassName("site-link");
+for (let index = 0; index < navs.length; index++) {
+  navs[index].addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+  });
+}
