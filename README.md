816193b9a12915307918517a8101addac378c17e
bryan <bryanhernandz102@gmail.com>
2022-11-20 22:48:47 -0500

    functions of validation form JS

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # portfolioToti
+
 Personal portfolio example to show competences implementing HTML and CSS concepts.
 👇🏼clique aqui
 https://soubryanh.github.io/portfolioToti/
+
+[Desafio de JavaScript:](https://share.vidyard.com/watch/MocjHG25CdwokBBzYthWoc?) Criar uma validação do formulário
--- assets/index.js
+++ assets/index.js
@@ -19,3 +19,61 @@ for (let index = 0; index < navs.length; index++) {
 }
 
 // validação do formulário
+
+//This simple validation just check if there is a value inside of each input.
+
+const form = document.getElementById('form');
+const subject = document.getElementById('subject');
+const contact = document.getElementById('contact1');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  //get the values from the inputs
+  //Add funct "trim" to remove whitespaces
+
+  const contactValue = contact.value.trim();
+  const subjectValue = subject.value.trim();
+  const messageValue = message.value.trim();
+
+  if (subjectValue === '') {
+    //show error message
+    //add error class
+    setErrorFor(subject, 'Subject cannot be blank');
+  } else {
+    //add success class
+    setSuccessFor(subject);
+  }
+
+  if (contactValue === '') {
+    setErrorFor(contact, 'Contact cannot be blank');
+  } else {
+    setSuccessFor(contact);
+  }
+
+  if (messageValue === '') {
+    setErrorFor(message, 'Message cannot be blank');
+  } else {
+    setSuccessFor(message);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector('small');
+  // add error message inside small
+  small.innerText = message;
+
+  // add error class
+  formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
